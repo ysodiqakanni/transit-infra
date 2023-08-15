469ca647c855d66bee359a78d5c05725bdeab68d
sodiq yusuff <ysodiqakanni@gmail.com>
2023-08-15 18:16:00 -0500

    fix path for modules cluster

--- main/main.tf
+++ main/main.tf
@@ -59,7 +59,7 @@ module "acr" {
 }
 
 module "cluster" {
-  source                = "./modules/cluster/"
+  source                = "../modules/cluster/"
   # serviceprinciple_id   = var.serviceprinciple_id
   # serviceprinciple_key  = var.serviceprinciple_key
   # ssh_key               = var.ssh_key
